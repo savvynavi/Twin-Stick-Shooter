4a0eb0366318bb308e5b4247abcfa0cb56e83059
Georgia Shepherd <fruitbat_08@hotmail.com>
2019-01-23 17:47:03 +1030

    deleted free bullets as they used out of date api stuff, added in bullets that spawn when right stick moved and go in the direction player is facing and will either despawn after 2 seconds or when they hit a collider

---
+++ Mighty Kingdom Programming Test - Twin Sticks/Assets/_Scripts/BulletBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+	public float speed;
+	public float aliveTime;
+	public float damage;
+
+	Vector3 moveDir = Vector3.zero;
+	Rigidbody rigidbody;
+	float timer;
+
+
+	// Use this for initialization
+	void Awake () {
+		rigidbody = GetComponent<Rigidbody>();
+		timer = Time.time + aliveTime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(transform.gameObject.layer == 8) {
+			rigidbody.MovePosition(transform.localPosition + transform.forward * Time.deltaTime * speed);
+		}
+		Destroy(transform.gameObject, aliveTime);
+	}
+
+	private void OnCollisionEnter(Collision collision) {
+		//if it hits something before alive time is up, it despawns
+		GetComponent<SphereCollider>().enabled = false;
+		GetComponent<MeshRenderer>().enabled = false;
+		Destroy(transform.gameObject, 0.1f);
+
+		if(collision.transform.gameObject.layer == 10) {
+
+		}
+	}
+}
--- Mighty Kingdom Programming Test - Twin Sticks/Assets/_Scripts/PlayerController.cs
+++ Mighty Kingdom Programming Test - Twin Sticks/Assets/_Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	public float Speed = 6;
 	public float rotationSpeed;
+	public Transform bullet;
+	public float bulletTimer;
 	public Thumbstick LeftStick;
 	public Thumbstick RightStick;
 
@@ -13,6 +15,7 @@ public class PlayerController : MonoBehaviour {
 	Animator animator = null;
 	Vector3 moveDir = Vector3.zero;
 	Quaternion NewRotation = Quaternion.identity;
+	float lastShot = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -23,26 +26,6 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//float vertical = Input.GetAxis("Vertical");
-		//float horizontal = Input.GetAxis("Horizontal");
-
-		//if(vertical != 0 || horizontal != 0) {
-		//	animator.SetBool("isIdle", false);
-		//} else {
-		//	animator.SetBool("isIdle", true);
-		//}
-
-
-		//////ORIGINAL SOLUTION!
-		//transform.Rotate(Vector3.up * horizontal * rotationSpeed);
-
-		//moveDir = new Vector3(0.0f, 0.0f, vertical);
-		//moveDir *= Speed;
-		//moveDir = transform.TransformDirection(moveDir);
-
-
-		//charaController.Move(moveDir * Time.deltaTime);
-
 		//THUMBSTICK CODE
 		float leftVertical = LeftStick.xAxis;
 		float leftHorizontal = LeftStick.yAxis;
@@ -50,26 +33,27 @@ public class PlayerController : MonoBehaviour {
 		float rightVertical = RightStick.xAxis;
 		float rightHorizontal = RightStick.yAxis;
 
+		//if any left stick movement, the player is no longer idle and will do walk anim
 		if(leftVertical != 0 || leftHorizontal != 0) {
 			animator.SetBool("isIdle", false);
 		} else {
 			animator.SetBool("isIdle", true);
 		}
 
-		//check if right stick is moving, if so rotate towards this
+		//check if right stick is moving, if so rotate towards this, if not rotate towards left stick
 		Vector3 lookDir = new Vector3(rightVertical, 0.0f, rightHorizontal);
 		moveDir = new Vector3(leftVertical, 0.0f, leftHorizontal);
 
 		if(lookDir.x != 0 || lookDir.z != 0) {
-			//transform.Rotate(Vector3.up * rightHorizontal * rotationSpeed);
 			NewRotation = Quaternion.LookRotation(lookDir, Vector3.up);
 			animator.SetBool("isShooting", true);
+			Shooting();
 		} else if (moveDir.x != 0 || moveDir.z != 0){
-			//transform.Rotate(Vector3.up * leftHorizontal * rotationSpeed);
 			NewRotation = Quaternion.LookRotation(moveDir, Vector3.up);
 			
 		}
 
+		//if no right stick movement, shooting anim stops
 		if(lookDir.x == 0 || lookDir.z == 0) {
 			animator.SetBool("isShooting", false);
 		}
@@ -77,27 +61,16 @@ public class PlayerController : MonoBehaviour {
 		transform.rotation = NewRotation;
 
 		moveDir *= Speed;
-		//moveDir = transform.TransformDirection(moveDir);
-
-		if(Input.GetKeyDown("space")) {
-			
-		} else {
-			
-		}
-		
-	}
-
-	//points player in direction of movement unless shooting, otherwise moves player around without rotating
-	void LeftStickMovement() {
-
-	}
-
-	//always rotates player in direction of right stick when used, also shoots
-	void RightStickMovement() {
-
 	}
 
 	void FixedUpdate() {
 		charaController.Move(moveDir * Time.deltaTime);
+	}
+
+	public void Shooting() {
+		if(Time.time - lastShot > bulletTimer) {
+			lastShot = Time.time;
+			Instantiate(bullet, transform.position + (transform.up * ((charaController.height / 4) * 3)) + (transform.forward * (charaController.radius + 1)), transform.rotation);
+		}
 	}
 }
